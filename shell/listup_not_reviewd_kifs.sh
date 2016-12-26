5d900e1bc63cefbfd1410010ff1b82e811427627
Takafumi Sakakibara <tsakaki@lr.pi.titech.ac.jp>
2016-12-26 13:57:25 +0900

    Modify: sort the result of listup_not_reviewed_kifs.sh

--- shell/listup_not_reviewd_kifs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-cat shogi_log.csv | awk -F, 'NF == 11 { print }'| grep -v "ぴよ将棋" | gawk -F, '$9 == "" || $9 == "\"\"" { print }'
-cat shogi_log.csv | grep "TODO"
-exit 0
---
+++ shell/listup_not_reviewed_kifs.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+#戦型を記入していない棋譜やTODOと書かれた棋譜をリストアップする。
+
+{
+    cat shogi_log.csv | awk -F, 'NF == 11 { print }'| grep -v "ぴよ将棋" | gawk -F, '$9 == "" || $9 == "\"\"" { print }'
+    cat shogi_log.csv | grep "TODO"
+} | sort -k1,1
+
+exit 0
