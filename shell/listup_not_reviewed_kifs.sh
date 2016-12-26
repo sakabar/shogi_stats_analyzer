5a89d54b1702fea87f716532cf3660cf807a2a82
Takafumi Sakakibara <tsakaki@lr.pi.titech.ac.jp>
2016-12-26 23:56:49 +0900

    Modify: uniq

--- shell/listup_not_reviewed_kifs.sh
+++ shell/listup_not_reviewed_kifs.sh
@@ -5,6 +5,6 @@
 {
     cat shogi_log.csv | awk -F, 'NF == 11 { print }'| grep -v "ぴよ将棋" | gawk -F, '$9 == "" || $9 == "\"\"" { print }'
     cat shogi_log.csv | grep "TODO"
-} | sort -k1,1
+} | sort -k1,1 | uniq
 
 exit 0
