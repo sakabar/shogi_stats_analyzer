635e79c8f018ef029495b424fa80db23c82fcf39
Takafumi Sakakibara <tsakaki@lr.pi.titech.ac.jp>
2016-12-27 13:09:45 +0900

    New: read_kif_from_clipboard

--- shell/check_win_percentage.sh
+++ shell/check_win_percentage.sh
@@ -25,4 +25,6 @@ cat $tmp_file | $py_cmd src/check_win_percentage.py 100 5  | column -t > win_per
 cat $tmp_file | $py_cmd src/check_win_percentage.py $log_size 5  | column -t > win_percentage_all.txt &
 wait
 
+shell/listup_not_reviewed_kifs.sh > not_reviewed_kifs.csv
+
 rm -rf $tmp_file
---
+++ shell/read_kif_from_clipboard.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+set -u
+
+#クリップボードに保存された文字列の中から棋譜ファイル名と思われる部分を抜き出して読み込み、
+#棋譜の中身をクリップボードに読み込む
+which "pbcopy" > /dev/null
+isMac=$?
+
+if [ $isMac -eq 0 ];then
+    :
+else
+    echo "$0 : use this command in Mac">&2
+    exit 1
+fi
+
+clipboard_text=$(mktemp -t tmp_file)
+pbpaste > $clipboard_text
+kif_file=$(cat $clipboard_text | grep -o -m 1 "[0-9]\{8\}_[0-9]\{4\}")".kif"
+
+if [[ $kif_file = ".kif" ]]; then
+    echo "Can't read kif_file name from:"$clipboard_text
+else
+    echo $kif_file >&2
+    cat kif/$kif_file | pbcopy
+fi
+
+rm -rf $clipboard_text
