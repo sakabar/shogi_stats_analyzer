eb65b75b6bdd55bf215ad0eb52963070c615bc65
Takafumi Sakakibara <tsakaki@lr.pi.titech.ac.jp>
2017-01-10 05:13:30 +0900

    Modify: error handling: when there is no 81Dojo kif

--- shell/read_piyo_kifs.sh
+++ shell/read_piyo_kifs.sh
@@ -13,14 +13,17 @@ mkdir -p $trash_dir
 #81Dojoの棋譜のファイル名から対局開始時間を読み取って、棋譜ファイル内に記入
 # 81Dojo-2016-12-31-14-10.kif
 # "開始日時：2016/12/31"の行 -> "開始日時：2016/12/31 14:10:00"に書き換え
-for f in 81Dojo-*.kif; do
-    nkf --overwrite -w -Lu $f
-    nf=$(cat $f | grep -m 1 "^開始日時" | awk '{printf("%d",NF)}')
-    if [ $nf -eq 1 ]; then
-        start_time=$(echo -n $f:t:r | cut -d '-' -f5,6 | tr '-' ':')":00"
-        gsed -i 's|^\(開始日時.*\)$|\1 '$start_time'|' $f
-    fi
-done
+cnt=$(ls -U *.kif  | grep -c "^81Dojo-")
+if [ $cnt -ge 1 ];then
+    for f in 81Dojo-*.kif; do
+        nkf --overwrite -w -Lu $f
+        nf=$(cat $f | grep -m 1 "^開始日時" | awk '{printf("%d",NF)}')
+        if [ $nf -eq 1 ]; then
+            start_time=$(echo -n $f:t:r | cut -d '-' -f5,6 | tr '-' ':')":00"
+            gsed -i 's|^\(開始日時.*\)$|\1 '$start_time'|' $f
+        fi
+    done
+fi
 
 ls -U | grep -e "^81Dojo-.*\.kif$" -e "^piyo_.*.kif$" | while read f; do
     shell/read_kif.sh $f
