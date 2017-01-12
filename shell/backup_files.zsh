09380b623e2b0573c81a29fd8db442197d1e4f6c
Takafumi Sakakibara <tsakaki@lr.pi.titech.ac.jp>
2017-01-12 14:00:14 +0900

    New: backup kif directory and shogi_log.csv

---
+++ shell/backup_files.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+set -u
+
+if [ $# -ne 0 ]; then
+    "$0 : Argument Error">&2
+    exit 1
+fi
+
+#kifディレクトリとshogi_log.csvをバックアップする
+backup_dir=backup/backup_$(date "+%Y%m%d_%H%M")
+mkdir -p $backup_dir
+cp -a kif shogi_log.csv $backup_dir
+tar zcf ${backup_dir}.tar.gz -C $backup_dir:h $backup_dir:t
+
+rm -rf $backup_dir
--- shell/check_win_percentage.sh
+++ shell/check_win_percentage.sh
@@ -7,25 +7,20 @@ if [ $# -ne 0 ]; then
     exit 1
 fi
 
-# if [ $# -ne 2 ]; then
-#     echo "$0 : Argument Error:<batch> <topn>">&2
-#     exit 1
-# fi
+tmp_csv_file=`mktemp -t tmp_csv_file`
+cat shogi_log.csv | grep -v "^#" | gawk -F',' 'NF == 11 {print $0}' | gawk -F',' '$6 != "\"接続\"" {print}' | gawk -F',' '$2 != "\"ぴよ将棋\"" {print}' | grep -v "^\"1棋譜ファイル名" | gawk -F, '$7 == "\"勝\"" || $7 == "\"負\"" { print }' | gawk -F, '$9 != "\"\"" && $9 != "" && $10 != "\"\"" && $10 != "" { print }' > $tmp_csv_file
 
-# readonly batch=$1 #50
-# readonly topn=$2
-
-tmp_file=`mktemp -t tmp_file`
-cat shogi_log.csv | grep -v "^#" | gawk -F',' 'NF == 11 {print $0}' | gawk -F',' '$6 != "\"接続\"" {print}' | gawk -F',' '$2 != "\"ぴよ将棋\"" {print}' | grep -v "^\"1棋譜ファイル名" | gawk -F, '$7 == "\"勝\"" || $7 == "\"負\"" { print }' | gawk -F, '$9 != "\"\"" && $9 != "" && $10 != "\"\"" && $10 != "" { print }' > $tmp_file
-
-log_size=$(cat $tmp_file | wc -l)
+log_size=$(cat $tmp_csv_file | wc -l)
 readonly py_cmd=/Users/sak/local/src/anaconda3/envs/py35con/bin/python3.5 #何故かanacondaでactivateしたコマンドが実行されないので直接指定
-cat $tmp_file | $py_cmd src/check_win_percentage.py  50 5  | column -t > win_percentage_b050.txt &
-cat $tmp_file | $py_cmd src/check_win_percentage.py 100 5  | column -t > win_percentage_b100.txt &
-cat $tmp_file | $py_cmd src/check_win_percentage.py $log_size 5  | column -t > win_percentage_all.txt &
+cat $tmp_csv_file | $py_cmd src/check_win_percentage.py  50 5  | column -t > win_percentage_b050.txt &
+cat $tmp_csv_file | $py_cmd src/check_win_percentage.py 100 5  | column -t > win_percentage_b100.txt &
+cat $tmp_csv_file | $py_cmd src/check_win_percentage.py $log_size 5  | column -t > win_percentage_all.txt &
 wait
 
 #まだ反省していない棋譜をリストアップ
 shell/listup_not_reviewed_kifs.sh
 
-rm -rf $tmp_file
+#現在のkifディレクトリのバックアップ
+shell/backup_files.zsh
+
+rm -rf $tmp_csv_file
