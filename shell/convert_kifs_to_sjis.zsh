9b5e00252ca53b7d4ad58270442f26f9d7830c25
Takafumi Sakakibara <tsakaki@lr.pi.titech.ac.jp>
2017-01-19 16:41:07 +0900

    Modify: make target dir when convert_kifs_to_sjis

--- shell/convert_kifs_to_sjis.zsh
+++ shell/convert_kifs_to_sjis.zsh
@@ -2,15 +2,18 @@
 
 source_dir=kif
 target_dir=kif_sjis
+[ ! -e kif_sjis ] && mkdir -p $target_dir
 
 for kif_file in $source_dir/*.*; do
     if [ -e $target_dir/$kif_file:t ]; then
         if [ $target_dir/$kif_file:t -ot $source_dir/$kif_file:t ]; then
+            echo "converted ${kif_file}"
             cat $kif_file | nkf -s -Lm > $target_dir/$kif_file:t
         else
             :
         fi
     else
+        echo "converted ${kif_file}"
         cat $kif_file | nkf -s -Lm > $target_dir/$kif_file:t
     fi
 done
